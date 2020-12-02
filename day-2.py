3c44cfbc1bed02c2dca973ac4cb8217ec3e85fd4
RayWiis <raywiis@cock.li>
2020-12-02 20:43:18 +0200

    Add day 2 problems and solutions

---
+++ day-2.py
@@ -0,0 +1,57 @@
+f = open('./day-2-problem.txt')
+passwords = [line.split(':') for line in f.read().splitlines()]
+
+def matches_rules_2(rule, password):
+    first, second, letter = parse_rule(rule)
+
+    if password[first] == letter and password[second] != letter:
+        return True
+
+    if password[first] != letter and password[second] == letter:
+            return True
+
+    return False
+
+def matches_rules(rule, password):
+    lower, upper, letter = parse_rule(rule)
+
+    letter_map = dict()
+    for l in password:
+        if l not in letter_map:
+            letter_map[l] = 0
+        letter_map[l] += 1
+
+    if letter not in letter_map:
+        if lower == 0:
+            return True
+        else:
+            return False
+
+    letter_count = letter_map[letter]
+    if letter_count <= upper and letter_count >= lower:
+        return True
+    else:
+        return False
+
+
+def parse_rule(ruleString):
+    [numbers, letter] = ruleString.split(' ')
+    [lower, upper] = numbers.split('-')
+
+    return int(lower), int(upper), letter
+
+count = 0
+for [rule, password] in passwords:
+    if matches_rules(rule, password):
+        count += 1
+
+print(count)
+
+count = 0
+for [rule, password] in passwords:
+    if matches_rules_2(rule, password):
+        count += 1
+
+print(count)
+
+
