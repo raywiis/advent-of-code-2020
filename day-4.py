922cde744d1391a8d2989485fd29a14f73a94458
RayWiis <raywiis@cock.li>
2020-12-05 12:29:22 +0200

    Add day 4 solutions

---
+++ day-4.py
@@ -0,0 +1,73 @@
+f = open('./day-4-problem.txt')
+
+batch = f.read().split('\n\n')
+batch = [e.split() for e in batch]
+batch = [dict([tuple(a.split(':')) for a in e]) for e in batch]
+
+def is_valid_passport(p):
+    if 'byr' in p and 'iyr' in p and 'eyr' in p and 'hgt' in p and 'hcl' in p and 'ecl' in p and 'pid' in p:
+        return True
+    return False
+
+def valid_fields(p):
+    bir = int(p['byr'])
+    iyr = int(p['iyr'])
+    eyr = int(p['eyr'])
+    hgt = p['hgt']
+
+    if bir < 1920 or bir > 2002:
+        return False
+
+    if iyr < 2010 or iyr > 2020:
+        return False
+
+    if eyr < 2020 or eyr > 2030:
+        return False
+
+    if hgt.endswith('cm'):
+        cm = int(hgt[:-2])
+        if cm < 150 or cm > 193:
+            return False
+
+    if hgt.endswith('in'):
+        inc = int(hgt[:-2])
+        if inc < 59 or inc > 76:
+            return False
+
+
+    hcl = p['hcl']
+    if not hcl.startswith('#') and len(hcl) != 7:
+        return False
+
+    try:
+        x = int('0x' + hcl[1:], 0)
+    except:
+        return False
+
+    if p['ecl'] not in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
+        return False
+
+    if len(p['pid']) != 9:
+        return False
+    try:
+        x = int(p['pid'])
+    except:
+        return False
+
+    if not (hgt.endswith('cm') or hgt.endswith('in')):
+        print(p)
+        return False
+
+    return True
+
+count = 0
+for p in batch:
+    if is_valid_passport(p):
+        if valid_fields(p):
+            count += 1
+        #print(p)
+
+# 154 - too high
+# 153 - 
+print(count)
+
