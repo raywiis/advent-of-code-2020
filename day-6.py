6484759d9d0fdcc0b0cbea21eb36166f3363a05e
RayWiis <raywiis@cock.li>
2020-12-07 20:09:58 +0200

    Add day 6 solutions

---
+++ day-6.py
@@ -0,0 +1,32 @@
+f = open('./day-6-problem.txt')
+data = f.read().split('\n\n')
+
+data = [a.splitlines() for a in data]
+
+def get_set_1(group):
+    total = set()
+
+    for person in group:
+        answers = set(person)
+        total = total.union(answers)
+
+    return total
+
+def get_set_2(group):
+    total = set(group[0])
+
+    for p in group:
+        answers = set(p)
+        total = total.intersection(answers)
+
+    return total
+
+
+counts = [len(get_set_1(g)) for g in data]
+
+print(sum(counts))
+
+counts = [len(get_set_2(g)) for g in data]
+
+print(sum(counts))
+
