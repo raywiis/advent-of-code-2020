85a6d36bf06d02b9c8a7797d373cab11050f4baf
RayWiis <raywiis@cock.li>
2020-12-10 22:13:34 +0200

    Add day 10 solutions

---
+++ day-10.py
@@ -0,0 +1,46 @@
+from functools import reduce
+f = open('./day-10-problem.txt')
+adapters = [int(i) for i in f.read().splitlines()]
+adapters.sort()
+
+cap = max(adapters) + 3
+
+diffs = [0,0,1]
+current = 0
+
+diffmap = []
+for i in adapters:
+   diff = i - current
+
+   assert(diff < 4 and diff > 0)
+
+   diffmap.append(diff)
+
+   current = i
+   diffs[diff - 1] += 1
+
+print(diffs[0] * diffs[2])
+
+cache = {
+    1: 1,
+    2: 2,
+    3: 4
+}
+
+def get_iters(target):
+    if target not in cache:
+        assert(target > 3)
+        cache[target] = sum(
+            [get_iters(target - i) for i in range(1, 4)]
+        )
+
+    return cache[target]
+
+diffmap = ''.join([str(i) for i in diffmap]).split('3')
+diffmap = list(filter(lambda x: x > 0, [len(x) for x in diffmap]))
+diffmap = [get_iters(i) for i in diffmap]
+
+target = reduce(lambda a, b: a * b, diffmap)
+
+print(target)
+
